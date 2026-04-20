a7b434b17f6521c7fbc24ff710a6ae85ac813e99
5elements Chatbot <dreypetroff@gmail.com>
2026-04-20 12:57:05 +0200

    fix: explicitly import React in ChatWidget to resolve React is not defined

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
 import toast, { Toaster } from 'react-hot-toast'
 
